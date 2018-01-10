1b9c7a3926b4cd0abd1c0e60f7489a4ecc6f014b
Igor Matos <igormatos.developer@gmail.com>
2018-01-10 10:15:21 -0200

    Finishing last classes about 2D-PLATFORM game

--- 2D-PLATFORM/Assets/PlayerPlatformerController.cs
+++ 2D-PLATFORM/Assets/PlayerPlatformerController.cs
@@ -7,10 +7,14 @@ public class PlayerPlatformerController : PhysicsObject
     public float jumpTakeOffSpeed = 7;
     public float maxSpeed = 7;
 
-    // Use this for initialization
-    void Start()
-    {
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,14 +22,27 @@ public class PlayerPlatformerController : PhysicsObject
     {
         Vector2 move = Vector2.zero;
         move.x = Input.GetAxis("Horizontal");
+
         if (Input.GetButtonDown("Jump") && grounded)
         {
             velocity.y = jumpTakeOffSpeed;
-        }else if (Input.GetButtonUp("Jump"))
+        }
+        else if (Input.GetButtonUp("Jump"))
         {
             if (velocity.y > 0)
                 velocity.y = velocity.y * .5f;
         }
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
         targetVelocity = move * maxSpeed;
+
+
     }
 }
